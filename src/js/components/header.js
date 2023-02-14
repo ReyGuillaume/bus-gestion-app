7575606c63d1681713cb2b6fd32e730b3dfb28a1
Guillaume Rey <grey02@orange.fr>
2023-02-14 18:29:36 +0100

    page agenda et modification de la navbar

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,11 +1,17 @@
 import { create } from "../main";
+import { toggleAccueil } from "../pages/accueil";
+import { toggleAgenda } from "../pages/agenda";
 
 export const createHeader = () => {
     const h = document.querySelector("#header")
     h.replaceChildren("")
     create("h1", h, "GoBus", ['title'])
+
     const nav = create("nav", h, null, ['navBar'])
-    const items = ['accueil', 'agenda', 'se connecter']
-    items.forEach(item => create("div", nav, item, ['navBar__item']))
+
+    create("div", nav, 'accueil', ['navBar__item']).addEventListener("click", toggleAccueil)
+    create("div", nav, 'Agenda', ['navBar__item']).addEventListener("click", toggleAgenda)
+    create("div", nav, 'Se connecter', ['navBar__item']).addEventListener("click", null)
+
     return h;
 }
