45f3b8270eb8f48dd5b5267bbc424d7d6a9973f0
Ameliorer <ameliehacque@gmail.com>
2023-02-22 23:38:11 +0100

    ajout du bouton pour ajouter un timeslots

--- src/js/pages/indisponibilitiForm.js
+++ src/js/pages/indisponibilitiForm.js
@@ -1,4 +1,5 @@
 import { create, createChamp } from "../main";
+import axios from 'axios';
 
 export const toggleIndisponibilitiForm = () => {
     const main = document.querySelector("#app")
@@ -9,8 +10,8 @@ export const toggleIndisponibilitiForm = () => {
 
     // Creation of the form
     const form = create("form", main)
-    form.setAttribute("method", "post");
-    form.setAttribute("action", "timeslots.php");
+    //form.setAttribute("method", "post");
+    //form.setAttribute("action", "");
 
     // Creation of each champ
     create("label", form, "Entrez la date de début de l'indisponibilité :");
@@ -23,9 +24,21 @@ export const toggleIndisponibilitiForm = () => {
 
 
     // Creation of submit button
-    var bouton = document.createElement("input");
-    bouton.setAttribute("type", "submit");
-    bouton.setAttribute("value", "Envoyer");
+    //var bouton = document.createElement("input");
+    //bouton.setAttribute("type", "submit");
+    //bouton.setAttribute("value", "Envoyer");
+    const bouton = create("button", form, "Envoyer")
+    bouton.addEventListener("click", function (event){
+        let idUser = 1;
+        let StartDateTime = document.querySelector("input[name='StartDateTime']").value;
+        let EndDateTime = document.querySelector("input[name='EndDateTime']").value;
+        console.log(StartDateTime);
+        console.log(EndDateTime);
+
+        axios.get(`timeslots/timeslots.php?function=create&beginning=${StartDateTime}&end=${EndDateTime}&type=${3}&users=${idUser}&buses=${0}`)
+
+
+    });
     form.appendChild(bouton);
 
 
