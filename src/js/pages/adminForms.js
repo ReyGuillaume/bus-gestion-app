99485ecc3f5f611346490eeeb246a9a591f19ad0
Mila Desmet <desmetmila@gmail.com>
2023-02-23 00:58:44 +0100

    Ajout de fonction radio et checkbox + avancement form

--- src/js/pages/adminForms.js
+++ src/js/pages/adminForms.js
@@ -1,9 +1,11 @@
-import { create, createChamp } from "../main";
+import { create, createChamp, createChampRadio } from "../main";
+
+import axios from 'axios';
 
 export const toggleAddCreneau = () => {
     const main = document.querySelector("#app")
     main.replaceChildren("")
-
+    
     create("h2", main, "Ajout de crénaux")
     create("p", main, " Rentrez les informations suivantes : ")
 
@@ -19,14 +21,30 @@ export const toggleAddCreneau = () => {
     create("label", form, "Entrez la date de fin du creneau :");
     createChamp(form, "datetime-local", "EndDateTime");
     create("br", form);
-    createChamp(form, type ="submit", value = "Envoyer");
+
+    
+
+    // Creation of the radio to define the type of the timeslot
+    var divRadio = create("div", form);
+    create("label", form, "Coisissez le type du créneau :");
+    axios.get(`timeslots/timeslots.php?function=types`).then((response)=>{
+        for(var type of response.data){
+            createChampRadio(divRadio, type.name , "type", type.id);
+            var label = create("label", divRadio, type.name);
+            label.setAttribute("for", type.name);
+          }
+    });
+
 
     // Creation of submit button
     var bouton = document.createElement("input");
     bouton.setAttribute("type", "submit");
     bouton.setAttribute("value", "Envoyer");
     form.appendChild(bouton);
-
+    
+    
+    
+    
 
     return main
 
