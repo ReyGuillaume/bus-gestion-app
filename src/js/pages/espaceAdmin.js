3e2c198310333501b7d9dcef1a372f29a4338fb0
Mila Desmet <desmetmila@gmail.com>
2023-02-23 11:05:03 +0100

    Formulaire supprimer creneau

--- src/js/pages/espaceAdmin.js
+++ src/js/pages/espaceAdmin.js
@@ -1,6 +1,6 @@
 import { create } from "../main";
 import { toggleAgenda } from "../pages/agenda";
-import { toggleAddCreneau } from "../pages/adminForms";
+import { toggleAddCreneau, toggleSupprimeUser } from "../pages/adminForms";
 
 
 export const toggleEspaceAdmin = () => {
@@ -14,9 +14,69 @@ export const toggleEspaceAdmin = () => {
 
     create("div", nav, 'Voir mon agenda', ['navBar__item']).addEventListener("click", toggleAgenda)
     create("div", nav, "Ajouter un creneau", ['navBar__item']).addEventListener("click", toggleAddCreneau)
-    create("div", nav, "Ajouter des participants à un créneau", ['navBar__item']).addEventListener("click", null)
-    create("div", nav, 'Gerer les utilisateurs', ['navBar__item']).addEventListener("click", null)
-    create("div", nav, 'Gerer les bus', ['navBar__item']).addEventListener("click", null)
+    create("div", nav, "Gerer les créneau", ['navBar__item']).addEventListener("click", toggleGestionTimeslots)
+    create("div", nav, 'Gerer les utilisateurs', ['navBar__item']).addEventListener("click", toggleGestionUsers)
+    create("div", nav, 'Gerer les bus', ['navBar__item']).addEventListener("click", toggleGestionBus)
+    
+    create("div", nav, 'Se deconnecter', ['navBar__item']).addEventListener("click", null)
+
+
+    return main
+}
+
+export const toggleGestionUsers = () => {
+    const main = document.querySelector("#app")
+    main.replaceChildren("")
+
+    create("h2", main, "Gestion des utilisateurs")
+    create("p", main, " Que souhaitez vous faire ? ")
+
+    const nav = create("nav", main, null, ['navBar'])
+
+    create("div", nav, 'Ajouter un utilisateur', ['navBar__item']).addEventListener("click", null)
+    create("div", nav, "Modifier un utilisateur", ['navBar__item']).addEventListener("click", null)
+    create("div", nav, "Supprimer un utilisateur", ['navBar__item']).addEventListener("click", toggleSupprimeUser)
+    create("div", nav, 'Retour', ['navBar__item']).addEventListener("click", toggleEspaceAdmin)
+    
+    create("div", nav, 'Se deconnecter', ['navBar__item']).addEventListener("click", null)
+
+
+    return main
+}
+
+export const toggleGestionBus = () => {
+    const main = document.querySelector("#app")
+    main.replaceChildren("")
+
+    create("h2", main, "Gestion des Bus")
+    create("p", main, " Que souhaitez vous faire ? ")
+
+    const nav = create("nav", main, null, ['navBar'])
+
+    create("div", nav, 'Ajouter un bus', ['navBar__item']).addEventListener("click", null)
+    create("div", nav, "Modifier un bus", ['navBar__item']).addEventListener("click", null)
+    create("div", nav, "Supprimer un bus", ['navBar__item']).addEventListener("click", null)
+    create("div", nav, 'Retour', ['navBar__item']).addEventListener("click", toggleEspaceAdmin)
+    
+    create("div", nav, 'Se deconnecter', ['navBar__item']).addEventListener("click", null)
+
+
+    return main
+}
+
+export const toggleGestionTimeslots = () => {
+    const main = document.querySelector("#app")
+    main.replaceChildren("")
+
+    create("h2", main, "Gestion des Creneau")
+    create("p", main, " Que souhaitez vous faire ? ")
+
+    const nav = create("nav", main, null, ['navBar'])
+
+    create("div", nav, "Ajouter un Creneau", ['navBar__item']).addEventListener("click", toggleAddCreneau)
+    create("div", nav, "Modifier un Creneau", ['navBar__item']).addEventListener("click", null)
+    create("div", nav, "Supprimer un Creneau", ['navBar__item']).addEventListener("click", null)
+    create("div", nav, 'Retour', ['navBar__item']).addEventListener("click", toggleEspaceAdmin)
     
     create("div", nav, 'Se deconnecter', ['navBar__item']).addEventListener("click", null)
 
