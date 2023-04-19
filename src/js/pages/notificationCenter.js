4bbca9b7b056d6ed67b5ac92c0be07e9a45d7fbe
Guillaume Rey <grey02@orange.fr>
2023-04-19 19:11:28 +0200

    amelioration centre notification

--- src/js/pages/notificationCenter.js
+++ src/js/pages/notificationCenter.js
@@ -54,39 +54,67 @@ function fetch_data (divAllNotif, id_user, mode){
 
             switch (mode) {
                 case "unread" :
-                    create("div", img, 'A').addEventListener("click", function() {archiveNotif(id_notif)} );
-                    create("div", img, 'R').addEventListener("click", function() {readNotif(id_notif)} );
+                    create("div", img, 'A').addEventListener("click", function(e) {
+                        e.stopPropagation()
+                        archiveNotif(id_notif)
+                        toggleNotificationCenter()
+                    } );
+                    create("div", img, 'R').addEventListener("click", function(e) {
+                        e.stopPropagation()
+                        readNotif(id_notif)
+                        toggleNotificationCenter()
+                    } );
                     break;
 
                 case "read" :
-                    create("div", img, 'A').addEventListener("click", function() {archiveNotif(id_notif)} );
-                    create("div", img, 'U').addEventListener("click", function() {unreadNotif(id_notif)} );
+                    create("div", img, 'A').addEventListener("click", function(e) {
+                        e.stopPropagation()
+                        archiveNotif(id_notif)
+                        toggleNotificationCenter()
+                    } );
+                    create("div", img, 'U').addEventListener("click", function(e) {
+                        e.stopPropagation()
+                        unreadNotif(id_notif)
+                        toggleNotificationCenter()
+                    } );
                     break;
 
                 case "archive" :
-                    create("div", img, 'R').addEventListener("click", function() {readNotif(id_notif)} );
-                    create("div", img, 'U').addEventListener("click", function() {unreadNotif(id_notif)} );
-                    break;
-
-                case "all" :
-                    create("div", img, 'A').addEventListener("click", function() {archiveNotif(id_notif)} );
-                    create("div", img, 'R').addEventListener("click", function() {readNotif(id_notif)} );
-                    create("div", img, 'U').addEventListener("click", function() {unreadNotif(id_notif)} );
+                    create("div", img, 'R').addEventListener("click", function(e) {
+                        e.stopPropagation()
+                        readNotif(id_notif)
+                        toggleNotificationCenter()
+                    } );
+                    create("div", img, 'U').addEventListener("click", function(e) {
+                        e.stopPropagation()
+                        unreadNotif(id_notif)
+                        toggleNotificationCenter()
+                    } );
                     break;
 
                 default :
-                    create("div", img, 'A').addEventListener("click", function() {archiveNotif(id_notif)} );
-                    create("div", img, 'R').addEventListener("click", function() {readNotif(id_notif)} );
-                    create("div", img, 'U').addEventListener("click", function() {unreadNotif(id_notif)} );
+                    create("div", img, 'A').addEventListener("click", function(e) {
+                        e.stopPropagation()
+                        archiveNotif(id_notif)
+                        toggleNotificationCenter()
+                    } );
+                    create("div", img, 'R').addEventListener("click", function(e) {
+                        e.stopPropagation()
+                        readNotif(id_notif)
+                        toggleNotificationCenter()
+                    } );
+                    create("div", img, 'U').addEventListener("click", function(e) {
+                        e.stopPropagation()
+                        unreadNotif(id_notif)
+                        toggleNotificationCenter()
+                    } );
                     break;
             }
-
             //Listener pour afficher la notification sur l'entièreté de la page
-            divNotif.addEventListener("click", (function(notif, divAllNotif,id) {return function (){
+            divNotif.addEventListener("click", function() {
                 showNotification(notif, divAllNotif)
-                readNotif(id);
-
-            };})(notif, divAllNotif,id_notif),true);
+                readNotif(id_notif);
+            })
 
         }
     });
@@ -118,21 +146,23 @@ export const toggleNotificationCenter = async () => {
     const nav = create("ul", main, null, ['navNotif']);
     let listeStatus = ["unread", "read", "archive", "all"];
 
+    const divAllNotif = create("div", main);
 
         for(var status of listeStatus){
-
+            
             /*--------------
-             Au clic du choix de type de créneau on affiche les autres infos à choisir
-             ---------------*/
+            Au clic du choix de type de créneau on affiche les autres infos à choisir
+            ---------------*/
             let li = create("li", nav, null, ['navNotif_item']);
             var radio = createChampRadio(li, status , "selectionStatus", status);
             radio.style.position = "fixed";
             radio.style.opacity = 0;
-
+            
             radio.addEventListener('click', async function () {
                 // Recuperation du type du créneau en création
                 var statusToHandle = statusSelected();
-                console.log(statusToHandle);
+                
+                
 
                 switch (statusToHandle) {
                     case 'unread' :
@@ -165,7 +195,6 @@ export const toggleNotificationCenter = async () => {
         }
 
 
-    const divAllNotif = create("div", main);
     divAllNotif.replaceChildren(await fetch_data(divAllNotif, id_user, "all"));
 
 
