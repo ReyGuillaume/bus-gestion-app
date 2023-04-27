995b387632dc7afde7a0b8d2d334d449ba2710f4
Guillaume Rey <grey02@orange.fr>
2023-04-27 18:22:48 +0200

    test of upload-artifact

--- src/js/__tests__/domManipulation.test.js
+++ src/js/__tests__/domManipulation.test.js
@@ -1,15 +1,15 @@
-// import {
-//     create,
-//     // createChamp,
-//     // createChampCheckbox,
-//     // createChampRadio,
-//     // toggleAlert,
-//     // toggleError
-// } from "../utils/domManipulation"
+import jsdom from "jest-environment-node"
+import {
+    create,
+    // createChamp,
+    // createChampCheckbox,
+    // createChampRadio,
+    // toggleAlert,
+    // toggleError
+} from "../utils/domManipulation"
 
+test("La fonction create() crée bien un élément dans le DOM", () => {
+    // const button = create("button", jsdom, "text of button", ['button', '1'], ['id', '2'])
 
-// test("La fonction create() crée bien un élément dans le DOM", () => {
-//     const button = create("button", document, "text of button", ['button', '1'], ['id', '2'])
-
-//     expect(document.children[0])
-// })
+    expect(true).not.toBeNull()
+})
