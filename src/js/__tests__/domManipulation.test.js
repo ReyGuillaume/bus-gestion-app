37ee3d602b400ad182f3778497f526e482ed60da
Guillaume Rey <grey02@orange.fr>
2023-04-27 18:10:41 +0200

    test of upload-artifact

--- src/js/__tests__/domManipulation.test.js
+++ src/js/__tests__/domManipulation.test.js
@@ -1,15 +1,15 @@
-import {
-    create,
-    // createChamp,
-    // createChampCheckbox,
-    // createChampRadio,
-    // toggleAlert,
-    // toggleError
-} from "../utils/domManipulation"
+// import {
+//     create,
+//     // createChamp,
+//     // createChampCheckbox,
+//     // createChampRadio,
+//     // toggleAlert,
+//     // toggleError
+// } from "../utils/domManipulation"
 
 
-test("La fonction create() crée bien un élément dans le DOM", () => {
-    const button = create("button", document, "text of button", ['button', '1'], ['id', '2'])
+// test("La fonction create() crée bien un élément dans le DOM", () => {
+//     const button = create("button", document, "text of button", ['button', '1'], ['id', '2'])
 
-    expect(document.children[0])
-})
+//     expect(document.children[0])
+// })
