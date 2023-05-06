bcf047c672ae2cfdd27c95e0b82e7eca92dc7ef1
Ameliorer <ameliehacque@gmail.com>
2023-05-06 19:13:10 +0200

    ajout des tables pour les réservations + crud

--- src/services/createTables.php
+++ src/services/createTables.php
@@ -217,6 +217,37 @@ function create_table_notification() {
     $stm = bdd()->query($sql);
 }
 
+/**
+ * Execute la requête SQL qui crée la table de reservation
+ */
+function create_table_reservation() {
+    $sql = "CREATE TABLE IF NOT EXISTS reservation (
+        `id_reserv` INT NOT NULL AUTO_INCREMENT,
+        `arretDepart` VARCHAR(200) NOT NULL,
+        `arretArrive` VARCHAR(500) NOT NULL,
+        `dateDepart` DATETIME NOT NULL,
+        `id_client` INT NOT NULL,
+        `etat` ENUM('attente','valide', 'refuse') DEFAULT 'attente',
+        CONSTRAINT pk_reservation PRIMARY KEY (id_reserv),
+        CONSTRAINT fk_reservation_user FOREIGN KEY (id_client) REFERENCES `user` (`id`)
+    )";
+    $stm = bdd()->query($sql);
+}
+
+/**
+ * Execute la requête SQL qui crée la table reservation_timeslot
+ */
+function create_table_reservation_timeslot() {
+    $sql = "CREATE TABLE IF NOT EXISTS reservation_timeslot (
+        `id_reservation` INT NOT NULL,
+        `id_timeslot` INT NOT NULL,
+        CONSTRAINT pk_reservation_timeslot PRIMARY KEY (id_reservation, id_timeslot),
+        CONSTRAINT fk_reservationtimesolt_reservation FOREIGN KEY (id_reservation) REFERENCES `reservation` (`id_reserv`),
+        CONSTRAINT fk_reservationtimesolt_timeslot FOREIGN KEY (id_timeslot) REFERENCES `timeslot` (`id`)
+    )";
+    $stm = bdd()->query($sql);
+}
+
 
 // ==================== Création des tables de la base ====================
 create_table_bus_type();
@@ -234,8 +265,10 @@ create_table_line_type();
 create_table_lineType_line();
 create_table_creneau_couverture();
 create_table_notification();
+create_table_reservation();
+create_table_reservation_timeslot();
 // ==================== Instanciation des types de créneaux ====================
-$timeSlotTypes = array('Conduite', 'Réunion', 'Indisponibilité');
+$timeSlotTypes = array('Conduite', 'Réunion', 'Indisponibilité', 'Réservation', 'Astreinte');
 
 foreach ($timeSlotTypes as $type) {
     $sql = "INSERT INTO TimeSlotType (`name`) VALUE ('{$type}')";
