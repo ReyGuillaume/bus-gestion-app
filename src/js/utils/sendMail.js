323ccd5a1e8e3257d902c8887dadd937b4f720fa
Ameliorer <ameliehacque@gmail.com>
2023-05-17 13:26:47 +0200

    notif

--- src/js/utils/sendMail.js
+++ src/js/utils/sendMail.js
@@ -23,13 +23,16 @@ function messageByTypeMail(typeMail, data=null) {
     let message = ""
     switch (typeMail){
         case "ConfirmReserv" :
-            message = "Nous sommes heureux de vous informer que votre réservation de bus avec GoBus a été validée. \n" +
+            message = "Nous sommes heureux de vous informer que votre réservation de bus avec GoBus a été validée avec succès. Votre bus partira de l'arrêt " +data.arretDepart + " à "
+                +data.debut+" le "+data.debutDate+" et arrivera à l'arrêt"+data.arretArrive+" à "+data.fin+" le "+data.finDate+".\n" +
                 "\n" +
                 "Veuillez vous connecter à votre compte sur notre site web et accéder à la section \"Gérer mes réservations\" pour visualiser toutes les informations relatives à votre réservation, notamment l'horaire de départ et d'arrivée prévue.\n" +
                 "\n" +
+                "Nous avons affecté un chauffeur expérimenté à votre bus pour vous assurer un voyage sûr et agréable. Notre équipe de conducteurs est formée pour offrir un service de qualité et pour prendre soin de nos passagers.\n" +
+                "\n" +
                 "Si vous avez des questions ou des préoccupations, n'hésitez pas à nous contacter. Nous sommes toujours prêts à vous aider.\n" +
                 "\n" +
-                "Nous vous remercions de votre confiance envers GoBus et nous sommes impatients de vous fournir un service de qualité supérieure.\n";
+                "Nous vous remercions de votre confiance envers GoBus et nous sommes impatients de vous fournir un service de qualité supérieure."
             break;
         case "ConfirmInscriptionConducteur":
             message = "Nous sommes heureux de vous confirmer que votre inscription en tant que conducteur de bus GoBus a été prise en compte. Vous avez désormais accès à tous les services nécessaires pour exercer votre travail avec succès.\n" +
@@ -43,6 +46,19 @@ function messageByTypeMail(typeMail, data=null) {
                 "Si vous avez des questions ou des préoccupations, n'hésitez pas à nous contacter. Nous sommes toujours prêts à vous aider.\n" +
                 "\n" +
                 "Encore une fois, merci de votre confiance envers GoBus. Nous sommes impatients de vous fournir un service de qualité supérieure."
+            break;
+        case "RefusInscriptionAbonne" :
+            message = "Nous avons bien reçu votre demande d'inscription en tant qu'abonné GoBus. Nous sommes désolés de vous informer que votre demande a été refusée.\n" +
+                "\n" +
+                "Notre équipe de gestion des abonnements a examiné votre demande et a déterminé que nous ne sommes pas en mesure de vous offrir les services d'abonnement pour le moment.\n" +
+                "\n" +
+                "Nous vous encourageons à réessayer à l'avenir si votre situation change et que vous souhaitez bénéficier des avantages de l'abonnement GoBus.\n" +
+                "\n" +
+                "Si vous avez des questions ou des préoccupations, n'hésitez pas à nous contacter. Nous sommes toujours prêts à vous aider.\n" +
+                "\n" +
+                "Nous sommes désolés de ne pas pouvoir répondre favorablement à votre demande cette fois-ci, mais nous vous remercions tout de même de l'intérêt que vous portez à GoBus.";
+            break;
+        case "":
             break;
         default :
             break;
@@ -58,6 +74,12 @@ function subjectByTypeMail(typeMail) {
             break;
         case "ConfirmInscriptionConducteur" :
             subject = "Confirmation de l'inscription en tant que conducteur de bus GoBus";
+            break;
+        case "RefusInscriptionAbonne" :
+            subject = "Refus de la demande d'inscription à GoBus";
+            break;
+        case "RefusReservationAbonne" :
+            subject = "Refus de la demande d'inscription à GoBus";
             break;
         default :
             break;
