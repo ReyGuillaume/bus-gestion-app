0af01e0245e9ec7fdda885a47c5c86ed9c362727
Guillaume Rey <grey02@orange.fr>
2023-05-18 21:12:02 +0200

    ajout de tests unitaires

--- src/js/pages/404.js
+++ src/js/pages/404.js
@@ -1,11 +1,8 @@
 import { create } from "../utils/domManipulation";
-import {sendMail} from "../utils/sendMail.js";
 
 const toggle404 = () => {
     const main = document.querySelector("#app")
     main.replaceChildren("")
-    //sendMail("ConfirmReserv",
-    //    {firstname : "Amélie", mail:"ameliehacque@gmail.com", login:"hacquea", arretDepart:"Les Ducs", arretArrive:"Technolac", finDate:"17/05/2023", debutDate:"17/05/2023", debut:"10:30", fin:"11:30"})
 
     create("h1", main, "404 not found")
     create("h3", main, "Vous êtes perdus ?")
